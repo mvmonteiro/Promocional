19c08a2a4e49288426e4c468cfe98ef71ddfe315
mvmonteiro <marcus.vinim@icloud.com>
2023-07-10 13:46:13 -0300

    feat: nova implementação da página FAQ.

--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,22 @@
-import Link from "../src/components/Link";
+import FaqScreen from '../src/screens/FaqScreen'
 
-export default function FAQPage() {
-  return(
-    <div>
-      <h1>estou no faq</h1>
-      <Link href="/">
-        ir para home
-      </Link>
-    </div>
-  )
+export default FaqScreen
+
+export async function getStaticProps() {
+  console.log('Roda somente em build time')
+  const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+  const faq = await fetch(FAQ_API_URL)
+    .then( (respostaDoServidor) => {
+      return respostaDoServidor.json();
+    })
+    .then( (resposta) => {
+      return resposta;
+    })
+
+  return {
+    props: {
+      qualquercoisa: 'que eu passar aqui',
+      faq,
+    },
+  };
 }
